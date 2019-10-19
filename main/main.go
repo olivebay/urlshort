00515e5283d9eb6c6fa0fb3482f662789fec999f
olivebay <olivebay84@gmail.com>
2019-10-19 16:27:53 +0100

    format changes in main.go

--- main/main.go
+++ main/main.go
@@ -1,46 +1,46 @@
 package main
 
- import (
-	 "fmt"
-	 "net/http"
+import (
+	"fmt"
+	"net/http"
 
-	 "github.com/olivebay/urlshort"
- )
+	"github.com/olivebay/urlshort"
+)
 
- func main(){
-	 mux := defaultMux()
+func main() {
+	mux := defaultMux()
 
-	 // Build the MapHandler using the mux as the fallback
-	 pathsToUrls := map[string]string{
+	//Build the MapHandler using the mux as the fallback
+	pathsToUrls := map[string]string{
 		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
 		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
-	 }
+	}
 
-	 mapHandler := urlshort.MapHandler(pathsToUrls, mux)
+	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
 
-	 //Build the YAMLHandlder using the mapHandler as the fallback
-	 yaml := `
+	//Build the YAMLHandlder using the mapHandler as the fallback
+	yaml := `
 - path: /urlshort
   url: https://github.com/gophercises/urlshort
 - path: /urlshort-final
   url: https://github.com/gophercises/urlshort/tree/solution
 `
 
-	 yamHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
-	 if err != nil {
-		 panic(err)
-	 }
+	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+	if err != nil {
+		panic(err)
+	}
 
-	 fmt.Println("Starting the server on :8888")
-	 http.ListenAndServe(":8888", yamHandler)
- }
+	fmt.Println("Starting the server on :8888")
+	http.ListenAndServe(":8888", yamlHandler)
+}
 
- func defaultMux() *http.ServeMux {
-	 mux := http.NewServeMux()
-	 mux.HandleFunc("/hello", hello)
-	 return mux
- }
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/hello", hello)
+	return mux
+}
 
- func hello(w http.ResponseWriter, r *http.Request) {
+func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, world!")
 }
