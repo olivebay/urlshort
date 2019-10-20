108b6de31b6157e874e17a6bb695aa6d7d407e5d
olivebay <olivebay84@gmail.com>
2019-10-20 14:27:33 +0100

    Added JSON handler

--- handler.go
+++ handler.go
@@ -1,7 +1,10 @@
 package urlshort
 
 import (
+	"fmt"
 	"net/http"
+
+	"encoding/json"
 
 	"gopkg.in/yaml.v2"
 )
@@ -12,7 +15,7 @@ import (
 // that each key in the map points to, in string format).
 // If the path is not provided in the map, then the fallback
 // http.Handler will be called instead.
-func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+func MapHandler(pathsToUrls map[string]string) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		path := r.URL.Path
 		if dest, ok := pathsToUrls[path]; ok {
@@ -25,8 +28,7 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 // YAMLHandler will parse the provided YAML and then return
 // an http.HandlerFunc (which also implements http.Handler)
 // that will attempt to map any paths to their corresponding
-// URL. If the path is not provided in the YAML, then the
-// fallback http.Handler will be called instead.
+// URL.
 //
 // YAML is expected to be in the format:
 //
@@ -35,34 +37,62 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 //
 // The only errors that can be returned all related to having
 // invalid YAML data.
-//
-// See MapHandler to create a similar http.HandlerFunc via
-// a mapping of paths to urls.
-func YAMLHandler(yaml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+func YAMLHandler(yaml []byte) (http.HandlerFunc, error) {
 	parsedYaml, err := parseYAML(yaml)
+
 	if err != nil {
 		return nil, err
 	}
 	pathMap := buildMap(parsedYaml)
-	return MapHandler(pathMap, fallback), nil
+	return MapHandler(pathMap), nil
 }
 
-type path struct {
-	Path string `yaml:"path`
-	URL  string `yaml:"url`
+// JSONHandler parses JSON data into a map, MapHandler then maps the path to the url
+func JSONHandler(json []byte) (http.HandlerFunc, error) {
+	parsedJSON, err := parseJSON(json)
+	fmt.Println(string(json))
+	if err != nil {
+		return nil, err
+	}
+	pathMap := buildMapJSON(parsedJSON)
+	return MapHandler(pathMap), nil
 }
 
-func parseYAML(unparsedYaml []byte) ([]path, error) {
-	var parsedYAML []path
+type mapper struct {
+	Path string `yaml:"path" json:"path"`
+	URL  string `yaml:"url"  json:"url"`
+}
 
-	err := yaml.Unmarshal(unparsedYaml, &parsedYAML)
+func parseYAML(data []byte) ([]mapper, error) {
+	var parsedYAML []mapper
+
+	err := yaml.Unmarshal(data, &parsedYAML)
 	if err != nil {
 		return nil, err
 	}
 	return parsedYAML, nil
 }
 
-func buildMap(data []path) map[string]string {
+func parseJSON(data []byte) ([]mapper, error) {
+	var parsedJSON []mapper
+
+	err := json.Unmarshal(data, &parsedJSON)
+	if err != nil {
+		return nil, err
+	}
+	return parsedJSON, nil
+}
+
+func buildMap(data []mapper) map[string]string {
+	urlsMap := make(map[string]string)
+
+	for _, s := range data {
+		urlsMap[s.Path] = s.URL
+	}
+	return urlsMap
+}
+
+func buildMapJSON(data []mapper) map[string]string {
 	urlsMap := make(map[string]string)
 
 	for _, s := range data {
